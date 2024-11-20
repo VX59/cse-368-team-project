e69b893cad69de0fb55006b7dc633c89fb56660b
officialSaqib <143649215+officialSaqib@users.noreply.github.com>
2024-11-19 19:01:57 -0500

    all interface methods for SDL events have 'isPushed' argument now

--- Src/sdl_lib.h
+++ Src/sdl_lib.h
@@ -61,10 +61,13 @@ typedef union SDL_Event
 struct SDL
 {
     int scancode_offset = 93;
+    __uint8_t SDL_RELEASED = 0;
     __uint8_t SDL_PRESSED = 1;
     __uint32_t SDL_KEYDOWN = 0x300;
+    __uint32_t SDL_KEYUP = 0x301;
     __uint32_t SDL_MOUSEMOTION = 0x400;
     __uint32_t SDL_MOUSEBUTTONDOWN = 0x401;
+    __uint32_t SDL_MOUSEBUTTONUP = 0x402;
     __uint32_t SDL_BUTTON_LEFT = 1;
     __uint32_t SDL_TOUCH_MOUSEID = ((__uint32_t)-1);
 
